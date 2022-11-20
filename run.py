403f41445a3b53c2254d8961c3d806113feac5e9
Jonah Mackey <jonahmackey699@gmail.com>
2022-11-20 15:48:17 -0500

    Added parameter to ignore given words when extracting topics

--- run.py
+++ run.py
@@ -7,30 +7,30 @@ import pandas as pd
 import numpy as np
 import umap
 from sklearn.cluster import DBSCAN
-from hdbscan import HDBSCAN
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.preprocessing import normalize
 
 from sentence_transformers import SentenceTransformer
 from cleantext import clean
 import nltk
+from nltk.corpus import stopwords
+
 nltk.download('punkt')
+nltk.download('stopwords')
 
 import argparse
 
 
-def clean_data(data: list):  
-    """Cleans data by removing stopwords, punctuation, and numbers. 
-    Splits the result into sentences. 
-    Returns a list of cleaned sentences.
+def clean_data(text_data: list):  
+    """Cleans the text_data and splits it up into sentences. 
 
     Args:
-        data (list): Text data stored in a list of strings.
+        text_data (list): Text data stored in a list of strings.
     """
     cleaned_data = []
 
-    for i in range(len(data)): 
-        text = data[i]
+    for i in range(len(text_data)): 
+        text = text_data[i]
         clean_text = clean(text)
         clean_text = clean_text.replace('*', '')
         clean_text = clean_text.replace('-', '')
@@ -52,7 +52,6 @@ def clean_data(data: list):
 def visualize(text_data: list, 
               embeddings: np.ndarray, 
               classes: list, 
-              title: str, 
               save_dir: str, 
               notebook: False):
     """Provides an interactive visualization of the clustered 2D UMAP embeddings and plots the clusters.
@@ -61,8 +60,8 @@ def visualize(text_data: list,
         text_data (list): Text data stored in a list of strings.
         embeddings (np.ndarray): The 2D UMAP embeddings of the text data.
         classes (np.ndarray): The cluster labels of the embeddings.
-        title (str): The title of the plot.
         save_dir (str): The directory to save the plot to.
+        notebook (bool): Whether to display the plot in a notebook.
     """
     # fix class labels
     class_names = ['-1_noise' if i == -1 else f'{i}_cluster' for i in classes]
@@ -80,14 +79,13 @@ def visualize(text_data: list,
         color=alt.Color('Cluster', scale=alt.Scale(scheme='tableau20')),
         tooltip=['Text']).properties(
             width=600, 
-            height=600, 
-            title=title).configure_legend(
+            height=600).configure_legend(
                 orient='right', 
                 titleFontSize=16,
                 labelFontSize=16,
                 ).interactive()
             
-    chart.save(save_dir + title + '_plot.html')
+    chart.save(save_dir + 'visual.html')
     
     if notebook:
         altair_viewer.display(chart, inline=True)
@@ -98,7 +96,7 @@ def visualize(text_data: list,
 def extract_topics(text_data: list, 
                    classes: list, 
                    apply_tfidf: bool, 
-                   title: str,
+                   ignore_words: list,
                    save_dir: str):
     """Extracts the most common words in each cluster.
     
@@ -106,13 +104,15 @@ def extract_topics(text_data: list,
         text_data (list): Text data stored in a list of strings.
         classes (np.ndarray): The cluster labels.
         apply_tfidf (bool): Whether to apply the TF-IDF to the within cluster word counts.
-        title (str): The title of the csv file.
+        ignore_words (list): A list of words to ignore when extracting the topics.
         save_dir (str): The directory to save the plot to.
     """
+    stop_words = stopwords.words('english') + ignore_words
+    
     unique_classes = np.unique(classes)
     
     # get bag of words for text data
-    vectorizer = CountVectorizer(stop_words='english')
+    vectorizer = CountVectorizer(stop_words=stop_words)
     text_bow = vectorizer.fit_transform(text_data).toarray() 
     
     # get vocabulary
@@ -151,36 +151,37 @@ def extract_topics(text_data: list,
         topics.append([unique_classes[i], size, top5])
         
     topics_df = pd.DataFrame(topics, columns=['class', 'size', 'topics'])
-    topics_df.to_csv(path_or_buf=save_dir + title + "_topics.csv", index=False)
+    topics_df.to_csv(path_or_buf=save_dir + "topics.csv", index=False)
     
     return topics_df 
 
 
-def run(data_path: str,
-        question_n: int,
-        model: str, 
-        embed_dim: int, 
-        eps: float, 
-        min_samples: int,
-        save_dir: str,
-        notebook: bool):
-    """Runs the topic modeling pipeline.
+def cluster(data_path: str,
+            question_n: int,
+            model: str, 
+            embed_dim: int, 
+            eps: float, 
+            min_samples: int,
+            save_dir: str,
+            notebook: bool):
+    """Runs the first phase of the topic modeling pipeline.
     
-    The pipeline consists of the following steps:
-    - Load and clean the data.
-    - Embed the data using a pretrained sentence transformer model.
+    This function completes of the following steps:
+    - Load and clean the text data.
+    - Embed the data using a pre-trained sentence transformer model.
     - Reduce the dimensionality of the embeddings using UMAP.
     - Cluster the embeddings using DBSCAN.
-    - Extract topics for each cluster.
     - Visualize the clusters.
     
     Args:
         data_path (str): The path to the .xlsx data.
+        question_n (int): The question number to analyze from the survey.
         model (str): The sentence transformer model to use.
         embed_dim (int): The dimension to reduce the embeddings to using UMAP before clustering.
         eps (float): The epsilon value to use for DBSCAN.
         min_samples (int): The minimum number of samples to use for DBSCAN.
         save_dir (str): The directory to save the results to.
+        notebook (bool): Whether to display the clusters in a notebook.
     """
     
     dataset = pd.read_excel(data_path, header=0)    
@@ -202,22 +203,20 @@ def run(data_path: str,
     
     # cluster embeddings
     clustering_model = DBSCAN(eps=eps, min_samples=min_samples, metric='euclidean')
-    # clustering_model = HDBSCAN(min_cluster_size=15, metric='euclidean')
     classes = clustering_model.fit_predict(embeddings)
 
+    # visualization
     if embed_dim > 2:
         reducer = umap.UMAP(n_components=2, metric='euclidean', random_state=0)
         embeddings = reducer.fit_transform(embeddings)
 
-    # visualization
     visualize(text_data=answers, 
               embeddings=embeddings, 
               classes=classes,
-              title=f'question_{question_n}', 
               save_dir=save_dir,
               notebook=notebook)
     
-    return (answers, classes)
+    return answers, classes
 
 
 if __name__ == '__main__':
@@ -253,17 +252,17 @@ if __name__ == '__main__':
     except:
         pass
     
-    answers, classes = run(data_path=args.data_path,
-                           question_n=args.question_n,
-                           model=args.model,
-                           embed_dim=args.embed_dim,
-                           eps=args.eps,
-                           min_samples=args.min_samples,
-                           save_dir=args.save_dir,
-                           notebook=False)
+    answers, classes = cluster(data_path=args.data_path,
+                                question_n=args.question_n,
+                                model=args.model,
+                                embed_dim=args.embed_dim,
+                                eps=args.eps,
+                                min_samples=args.min_samples,
+                                save_dir=args.save_dir,
+                                notebook=False)
     
     topics = extract_topics(text_data=answers, 
                             classes=classes, 
                             apply_tfidf=True, 
-                            title=f'question_{args.question_n}',
+                            ignore_words=['expect'],
                             save_dir=args.save_dir)
