04f5315bf33a559fd3096c3954025c7a21b3c1a2
Jonah Mackey <jonahmackey699@gmail.com>
2023-04-18 21:26:34 -0700

    Finalized code and README

--- README.md
+++ README.md
@@ -1,80 +1,31 @@
 # Assessing Student Feedback from Large Class Surveys via Topic Modeling
 
-Notebook (Sentence Transformers) [![Open In Colab][colab-badge]][notebook]
-
-Notebook (Cohere) [![Open In Colab][colab-badge]][notebook-cohere]
+Notebook [![Open In Colab][colab-badge]][notebook]
 
 [notebook]: <https://colab.research.google.com/drive/1mTNwulr2rKcdm2j9kCujaGN0tEdp_9ua?usp=sharing>
-[notebook-cohere]: <https://colab.research.google.com/drive/1H7nK2TOmENLKqLq9ogOoXzL-4UI9Zsor?usp=sharing>
 [colab-badge]: <https://colab.research.google.com/assets/colab-badge.svg>
 
 ---
 
 ## About
 
-This is a topic modeling tool designed to help professors at the University of Toronto assess student feedback from surveys in large classes they are instructing. The Colab notebooks linked above provide an interface for easy use of the tool that gives an interpretable and interactive visualization of the results.
+This is a topic modeling tool designed to help professors at the University of Toronto assess student feedback from surveys in large classes they are instructing. The Colab notebooks linked above provide an interface for easy use of the tool.
 
 By providing surveys to students in their class, professors can get feedback on various things. However, for large classes, it is difficult and time-consuming to assess this feedback simply by reading the survey answers, as there are many submissions. Using this topic modeling tool, professors can painlessly assess the feedback from surveys in classes with 1000+ students in a few seconds.
 
 The topics that the algorithm outputs will highlight recurring ideas discovered in the survey answers in an interpretable manner. 
 
-
 ## How It Works
 
-The algorithm can be broken into 5 main steps.
+The algorithm can be broken into 4 main steps.
 
 1. **Text preprocessing:** Given a dataset containing survey answers, the algorithm first cleans and preprocesses the text. The text is broken up into sentences.
 
-2. **Embedding:** Using a pre-trained transformer model from [Sentence Transformers](https://www.sbert.net/docs/pretrained_models.html) or [Cohere](https://cohere.ai/), these sentences are embedded into a higher dimensional semantic space and converted into vectors. 
+2. **Embedding:** Using a pre-trained transformer model from [Sentence Transformers](https://www.sbert.net/docs/pretrained_models.html), these sentences are embedded into a higher dimensional semantic space and converted into vectors. 
 
-3. **Dimension reduction:** The algorithm then reduces the dimensionality of these sentence embeddings using [UMAP](https://umap-learn.readthedocs.io/en/latest/index.html#). This will allow for more visually interperetable results.
+3. **Clustering:** These lower dimensional embeddings are then clustered using the [DBSCAN algorithm](https://scikit-learn.org/stable/modules/clustering.html#dbscan). 
 
-4. **Clustering:** These lower dimensional embeddings are then clustered using the [DBSCAN algorithm](https://scikit-learn.org/stable/modules/clustering.html#dbscan). 
-
-5. **Topic extraction:** Topics are extracted for each cluster by computing the within cluster unigram frequencies, applying the class TF-IDF, then selecting the most frequent words. 
-
-
-## Example Usage
-
-The excel file [./example/classroom_norms.xlsx](./example/classroom_norms.xlsx) contains a student survey from the course "MAT188: Linear Algebra for Engineers" during Fall 2022. In this example, we apply the topic modeling algorithm to this dataset.
-
-**The survey question:** 
-"Our goal is to foster a safe and encouraging environment in which you all feel comfortable working together and sharing your thoughts with each other. To make that happen, what do you need or expect from yourself?"
-
-### Parameters
-- **question_n**: 0
-- **model:** all-MiniLM-L12-v2
-- **embed_dim**: 2 
-- **eps**: 0.35 
-- **min_samples**: 10
-
-### Results
-
-|class|size|topics                                                       |
-|-----|----|-------------------------------------------------------------|
-|-1   |109 |need_get_help_stay_organized                                 |
-|0    |39  |course_class_linear_professor_algebra                        |
-|1    |111 |help_others_questions_open_ask                               |
-|2    |14  |mistakes_make_need_afraid_first                              |
-|3    |20  |assignments_complete_early_sure_time                         |
-|4    |35  |would_thoughts_like_others_peers                             |
-|5    |66  |prepared_lectures_material_come_class                        |
-|6    |19  |organized_top_stay_work_everything                           |
-|7    |19  |inclusive_zone_comfort_others_nonjudgmental                  |
-|8    |11  |ask_questions_hours_office_lost                              |
-|9    |10  |procrastinate_procrastination_punctual_procrastinating_timely|
-|10   |21  |respect_respectful_learn_level_openminded                    |
-|11   |20  |group_ideas_participation_peers_active                       |
-|12   |20  |class_participate_actively_discussions_friends               |
-|13   |20  |learning_skills_ideas_environment_peers                      |
-|14   |10  |actually_classes_grades_time_attend                          |
-|15   |12  |time_management_assignments_ensure_good                      |
-
-### Visualization
-
-<img src="./example/visualization.png" alt="image" width="500"/>
-
-To visualize this plot interactively, download [this .html file](./example/visual.html) and open it in your browser.
+4. **Topic extraction:** Topics are extracted for each cluster by computing the within cluster unigram frequencies, applying the class TF-IDF, then selecting the most frequent words. 
 
 ## Acknowledgements
 
--- run.py
+++ run.py
@@ -1,12 +1,8 @@
 import os
 
-import altair as alt
-import altair_viewer
-
 import pandas as pd
 import numpy as np
-import umap
-from sklearn.cluster import DBSCAN
+from sklearn.cluster import AgglomerativeClustering
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.preprocessing import normalize
 
@@ -21,7 +17,7 @@ nltk.download('stopwords')
 import argparse
 
 
-def clean_data(text_data: list):  
+def clean_data(text_data: list, sent: bool):  
     """Cleans the text_data and splits it up into sentences.
 
     Args:
@@ -38,67 +34,28 @@ def clean_data(text_data: list):
         clean_text = clean_text.replace('..', '.')
         clean_text = clean_text.strip()
         clean_text = clean_text.replace('  ', ' ')
+        if clean_text == '':
+            continue
         if clean_text[0] == ' ':
             clean_text = clean_text[1:]
         if clean_text[-1] != '.':
             clean_text = clean_text + '.'
             
-        clean_text = nltk.tokenize.sent_tokenize(clean_text)        
+        if sent:
+            clean_text = nltk.tokenize.sent_tokenize(clean_text)        
+        else: 
+            clean_text = [clean_text]
+        
         cleaned_data += clean_text
     
     return cleaned_data
 
-
-def visualize(text_data: list, 
-              embeddings: np.ndarray, 
-              classes: list, 
-              save_dir: str, 
-              notebook: False):
-    """Provides an interactive visualization of the clustered 2D UMAP embeddings and plots the clusters.
-    
-    Args:
-        text_data (list): Text data stored in a list of strings.
-        embeddings (np.ndarray): The 2D UMAP embeddings of the text data.
-        classes (np.ndarray): The cluster labels of the embeddings.
-        save_dir (str): The directory to save the plot to.
-        notebook (bool): Whether to display the plot in a notebook.
-    """
-    # fix class labels
-    class_names = ['-1_noise' if i == -1 else f'{i}_cluster' for i in classes]
-    
-    # collect data into data frame
-    df = pd.concat([pd.DataFrame(data=text_data, columns=['Text']),  
-                    pd.DataFrame(data=embeddings[:, 0], columns=['X']),
-                    pd.DataFrame(data=embeddings[:, 1], columns=['Y']), 
-                    pd.DataFrame(data=class_names, columns=['Cluster'])], axis=1)
-    
-    # create interactive chart for data
-    chart = alt.Chart(df).mark_circle(size=150).encode(
-        x=alt.X('X', scale=alt.Scale(zero=False), axis=alt.Axis(labels=False, ticks=False, domain=False)),
-        y=alt.Y('Y', scale=alt.Scale(zero=False), axis=alt.Axis(labels=False, ticks=False, domain=False)),
-        color=alt.Color('Cluster', scale=alt.Scale(scheme='tableau20')),
-        tooltip=['Text']).properties(
-            width=600, 
-            height=600).configure_legend(
-                orient='right', 
-                titleFontSize=16,
-                labelFontSize=16,
-                ).interactive()
-            
-    chart.save(save_dir + 'visual.html')
-    
-    if notebook:
-        altair_viewer.display(chart, inline=True)
-    else:
-        altair_viewer.show(chart)
-    
     
 def extract_topics(text_data: list, 
                    classes: list, 
                    apply_tfidf: bool, 
-                   ignore_words: list,
-                   save_dir: str):
-    """Extracts the most common words in each cluster.
+                   ignore_words: list):
+    """Extracts the top 5 unigrams from each cluster.
     
     Args:
         text_data (list): Text data stored in a list of strings.
@@ -145,102 +102,125 @@ def extract_topics(text_data: list,
         top5.reverse()
         top5 = '_'.join(top5)
         
-        # get cluster size
-        size = classes[classes == unique_classes[i]].shape[0]
-        
-        topics.append([unique_classes[i], size, top5])
-        
-    topics_df = pd.DataFrame(topics, columns=['class', 'size', 'topics'])
-    topics_df.to_csv(path_or_buf=save_dir + "topics.csv", index=False)
+        topics.append(top5)
     
-    return topics_df 
+    return topics
 
 
 def cluster(data_path: str,
-            question_n: int,
-            model: str, 
-            embed_dim: int, 
-            eps: float, 
-            min_samples: int,
-            save_dir: str,
-            notebook: bool):
-    """Runs the first phase of the topic modeling pipeline.
-    
-    This function completes of the following steps:
-    - Load and clean the text data.
-    - Embed the data using a pre-trained sentence transformer model.
-    - Reduce the dimensionality of the embeddings using UMAP.
-    - Cluster the embeddings using DBSCAN.
-    - Visualize the clusters.
+            model: str,  
+            sent: bool,
+            thresh: float):
+    """Embeds and clusters the text data.
     
     Args:
         data_path (str): The path to the .xlsx data.
-        question_n (int): The question number to analyze from the survey.
         model (str): The sentence transformer model to use.
-        embed_dim (int): The dimension to reduce the embeddings to using UMAP before clustering.
-        eps (float): The epsilon value to use for DBSCAN.
-        min_samples (int): The minimum number of samples to use for DBSCAN.
-        save_dir (str): The directory to save the results to.
-        notebook (bool): Whether to display the clusters in a notebook.
+        sent (bool): Whether to split the text into sentences.
+        thresh (float): The threshold to use for agglomerative clustering.
     """
     
     dataset = pd.read_excel(data_path, header=0)    
-    question = dataset.columns[question_n]
+    question = dataset.columns[0]
     
     print('QUESTION:\n    ', f'"{question}"')
 
     # clean data
-    answers = dataset[dataset.columns[question_n]].dropna().tolist()
-    answers = clean_data(answers) 
-    
+    responses = dataset[dataset.columns[0]].dropna().tolist()
+    responses = clean_data(responses, sent=sent) 
+
     # embed data
     embedding_model = SentenceTransformer(model)
-    embeddings = embedding_model.encode(answers) 
-    
-    # reduce dimensionality of embeddings
-    reducer = umap.UMAP(n_components=embed_dim, metric='cosine', random_state=0)
-    embeddings = reducer.fit_transform(embeddings) 
+    embeddings = embedding_model.encode(responses) 
+    embeddings = embeddings / np.linalg.norm(embeddings, axis=1, keepdims=True)
     
     # cluster embeddings
-    clustering_model = DBSCAN(eps=eps, min_samples=min_samples, metric='euclidean')
-    classes = clustering_model.fit_predict(embeddings)
+    clustering_model = AgglomerativeClustering(n_clusters=None, 
+                                               distance_threshold=thresh, 
+                                               metric='euclidean',
+                                               linkage='ward')
+    classes = clustering_model.fit_predict(embeddings) # (556,)
     
-    # visualization
-    if embed_dim > 2:
-        reducer = umap.UMAP(n_components=2, metric='euclidean', random_state=0)
-        embeddings = reducer.fit_transform(embeddings)
+    return responses, classes, embeddings
 
-    visualize(text_data=answers, 
-              embeddings=embeddings, 
-              classes=classes,
-              save_dir=save_dir,
-              notebook=notebook)
+def save_results(responses: list, 
+                 classes: list, 
+                 embeddings: np.ndarray,
+                 topics: list,
+                 save_dir: str):
+    """Saves results to a .txt file. For each cluster, results are saved in the following format:
     
-    return answers, classes
+    CLUSTER <cluster_id> | SIZE <cluster_size> | TOPIC <cluster_topics>:
+        <response_1> - <response_5>
+        ...
+        <response_n-5> - <response_n>
+    
+    where the responses are sorted by distance from the cluster mean.
+    
+    Args:
+        responses (list): The list of responses.
+        classes (np.ndarray): The cluster labels.
+        embeddings (np.ndarray): The text embeddings.
+        topics (list): The topics for each cluster.
+        save_dir (str): The directory to save the results to.
+    """
+    responses = np.array(responses)
+    classes = np.array(classes)
+    topics = np.array(topics)
+    
+    # sort clusters by size
+    unique_classes = np.unique(classes)
+    cluster_sizes = np.array([len(embeddings[classes == i]) for i in unique_classes])
+    cluster_sorted = np.flip(np.argsort(cluster_sizes)) # shape? 
+    
+    result = ''
+    for i in range(len(unique_classes)):
+        cluster_idx = cluster_sorted[i]
+        
+        # get cluster and responses
+        cluster = embeddings[classes == cluster_idx]
+        cluster_responses = responses[classes == cluster_idx]
+        
+        # compute distance between each vector and the mean vector
+        cluster_mean = np.mean(cluster, axis=0)
+        distances = np.linalg.norm(cluster - cluster_mean, axis=1)
+        
+        # sort the indices based on the distance in ascending order
+        sorted_indices = np.argsort(distances)
+
+        # aggregate results
+        result += f'CLUSTER {cluster_idx} | SIZE {len(cluster)} | TOPICS {topics[cluster_idx]}:\n'
+        for sentence_id in sorted_indices[:5]:
+            result += f'\t{cluster_responses[sentence_id]}\n'
+        result += "\t...\n" 
+        for sentence_id in sorted_indices[-5:]:
+            result += f'\t{cluster_responses[sentence_id]}\n'
+        result += "\n"
+    
+    # save results to a .txt file
+    with open(save_dir + 'results.txt', 'w') as f:
+        f.write(result)
+        
+    return result
 
 
 if __name__ == '__main__':
-    # python run.py --data_path ./classroom_norms.xlsx --question_n 0 --model all-MiniLM-L12-v2 --embed_dim 2 --eps 0.4 --min_samples 15 --save_dir ./example/
-    
     parser = argparse.ArgumentParser()
     
-    parser.add_argument('--data_path', type=str, default='./example/classroom_norms.xlsx', 
-                        help='Path to excel (.xlsx) file containing survey answers.')
+    parser.add_argument('--data_path', type=str, default='./datasets/expect_from_peers.xlsx', 
+                        help='Path to excel (.xlsx) file containing survey responses.')
     
-    parser.add_argument('--question_n', type=int, default=0, 
-                        help='Question number to analyze from the survey.')
+    parser.add_argument('--sent', type=str, default='true', 
+                        help='Whether or not to split up the input text into sentences.')
+    
+    parser.add_argument('--ignore_words', nargs='+', default=['expect', 'peers'],
+                        help='Words to ignore when extracting cluster topics.')
     
     parser.add_argument('--model', type=str, default='all-MiniLM-L12-v2', 
                         help='The embedding model from sentence-transformers.')
-    
-    parser.add_argument('--embed_dim', type=int, default=2, 
-                        help='The dimension to reduce the embeddings to for clustering.')
-    
-    parser.add_argument('--eps', type=float, default=0.35, 
-                        help='The epsilon value to use for DBSCAN.')
-    
-    parser.add_argument('--min_samples', type=int, default=10, 
-                        help='The minimum number of samples to use for DBSCAN.')
+  
+    parser.add_argument('--thresh', type=float, default=2.5,
+                        help='The threshold to use for agglomerative clustering.')
     
     parser.add_argument('--save_dir', type=str, default='./example/', 
                         help='The directory to save results in.')
@@ -252,18 +232,29 @@ if __name__ == '__main__':
     except:
         pass
     
-    answers, classes = cluster(data_path=args.data_path,
-                                question_n=args.question_n,
-                                model=args.model,
-                                embed_dim=args.embed_dim,
-                                eps=args.eps,
-                                min_samples=args.min_samples,
-                                save_dir=args.save_dir,
-                                notebook=False)
+    sent = (args.sent == 'true')
     
-    topics = extract_topics(text_data=answers, 
+    # clustering
+    responses, classes, embeddings = cluster(data_path=args.data_path,
+                                           model=args.model,
+                                           sent=sent,
+                                           thresh=args.thresh)
+    
+    # extract topics
+    topics = extract_topics(text_data=responses, 
                             classes=classes, 
                             apply_tfidf=True, 
-                            ignore_words=['expect'],
-                            save_dir=args.save_dir)
+                            ignore_words=args.ignore_words)
     
+    # save results
+    result = save_results(responses=responses,
+                          classes=classes,
+                          embeddings=embeddings,
+                          topics=topics,
+                          save_dir=args.save_dir)
+    
+    print(result)
+    
+    with open(args.save_dir + 'params.txt', 'w') as f:
+        f.write(f"Parameters:\n\ndata_path: {args.data_path}\nsent: {sent}\nignore_words: {args.ignore_words}\nmodel: {args.model}\nthresh: {args.thresh}\n")
+        
